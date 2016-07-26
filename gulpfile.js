463075414430f3b642647db9450d4537306db2fd
ArtCSCH <artcsch@outlook.com>
2016-07-25 22:38:05 -0500

    Se crea el servidor local con webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,22 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 
-var 
+var config ={
+  html:{
+    watch: './build/*.html'
+  }
+};
+gulp.task('server', function(){
+  gulp.src('./build')
+    .pipe(webserver({
+      host: '0.0.0.0',
+      port: 9090,
+      livereload: {enable: true}
+    }));
+});
+
+gulp.task('watch', function(){
+  gulp.watch(config.html.watch);
+});
+
+gulp.task('default', ['server', 'watch']);
