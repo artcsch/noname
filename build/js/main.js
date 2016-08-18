51c51149c5232012035d0b820494ba7bc7dba7a2
ArtCSCH <artcsch@outlook.com>
2016-08-18 00:10:32 -0500

    se termina configurar page.js

--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,11 @@
 require('jquery');
+var page = require('page');
+var main = document.getElementById('nav-mobile');
+
 $(".button-collapse").sideNav();
+
+page('/', function(ctx, next){
+  main.innerHTML = 'Inicio <a href="/Datos">Datos</a>';
+});
+
+page();
